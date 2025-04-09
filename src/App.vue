c8a35425807e9f229098761113c66ae3cf4bbea2
Dimar Tarmizi <dimartarmizi@gmail.com>
2025-04-09 21:36:28 +0700

    remove gradient animation from App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-gradient-to-br from-gray-900 via-black to-gray-800 min-h-screen text-white animate-gradient">
+  <div class="bg-gradient-to-br from-gray-900 via-black to-gray-800 min-h-screen text-white">
     <Navbar />
     <div class="max-w-7xl mx-auto px-4">
       <router-view />
@@ -14,23 +14,6 @@ import Navbar from './components/Navbar.vue'
 <style>
 @import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css');
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap');
-
-@keyframes gradientAnimation {
-  0% {
-    background-position: 0% 50%;
-  }
-  50% {
-    background-position: 100% 50%;
-  }
-  100% {
-    background-position: 0% 50%;
-  }
-}
-
-.animate-gradient {
-  background-size: 200% 200%;
-  animation: gradientAnimation 6s ease infinite;
-}
 
 body {
   font-family: 'Poppins', sans-serif;
